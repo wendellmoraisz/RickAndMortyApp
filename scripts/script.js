71d0de53d286a9654bbbcff0172e9431f0a465e7
Wendell <wendellmorais3319@gmail.com>
2022-04-27 08:50:46 -0300

    improvements

--- scripts/script.js
+++ scripts/script.js
@@ -1,93 +1,84 @@
 const container = document.querySelector('.container')
 
-function insertInPage(response) {
-        let card = document.createElement('div')
-        card.classList.add('character-card')
-
-        let img = document.createElement('img')
-        img.src = response.image
-        card.appendChild(img)
-
-        let h2 = document.createElement('h2')
-        h2.textContent = response.name
-        card.appendChild(h2)
-
-        let pStatus = document.createElement('p')
-        pStatus.classList.add('status-wrapper')
-        let spanByStatus = document.createElement('span')
-        spanByStatus.classList.add('status')
-        spanByStatus.classList.add(response.status)
-        pStatus.appendChild(spanByStatus)
-        pStatus.append(`${response.status} - ${response.species}`)
-        card.appendChild(pStatus)
-        
-        let pLocation = document.createElement('p')
-
-        let spanLocation = document.createElement('span')
-        spanLocation.classList.add('location')
-        spanLocation.append('Last know location: ')
-        pLocation.appendChild(spanLocation)
-        pLocation.append(response.location.name)
-        card.appendChild(pLocation)
-        
-        container.appendChild(card)
+let charactersOfTheDay = []
+for (let i = 1; i < 42; i++) {
+    fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+        .then(res => res.json())
+        .then(json => charactersOfTheDay.push(json))
+        .then(() => {
+            if (charactersOfTheDay.length == 41) {
+                for (let i = 0; i < 3; i++)
+                    insertInPage(charactersOfTheDay[Math.floor(Math.random() * 41)].results[Math.floor(Math.random() * 19)])
+            }
+        })
 }
 
-let charactersOfTheDay = []
-    for(let i = 1; i < 42; i++){
-       fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-       .then(res => res.json())
-       .then(json => charactersOfTheDay.push(json))
-       .then(() => {
-        if(charactersOfTheDay.length == 41){
-            for(let i = 0; i < 3; i++)
-            insertInPage(charactersOfTheDay[Math.floor(Math.random() * 41)].results[Math.floor(Math.random() * 19)])
-        }   
-    })
-   }
+function insertInPage(response) {
+    let card = document.createElement('div')
+    card.classList.add('character-card')
 
-document.getElementById('characters').onclick = () => setAllCharacters()
+    let img = document.createElement('img')
+    img.src = response.image
+    card.appendChild(img)
 
-   function setAllCharacters(){
-       container.innerHTML = ''
-       let allCharacters = []
-    for(let i = 1; i < 42; i++){
-        fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-        .then(res => res.json())
-        .then(json => allCharacters.push(json))
-        .then(() => {
-         if(allCharacters.length == 41){
-             for(let i = 0; i < 10; i++){
-                 allCharacters[i].results.forEach(res => {
-                     insertInPage(res)
-                 })
-             }
-         }
-     })
+    let h2 = document.createElement('h2')
+    h2.textContent = response.name
+    card.appendChild(h2)
+
+    let pStatus = document.createElement('p')
+    pStatus.classList.add('status-wrapper')
+    let spanByStatus = document.createElement('span')
+    spanByStatus.classList.add('status')
+    spanByStatus.classList.add(response.status)
+    pStatus.appendChild(spanByStatus)
+    pStatus.append(`${response.status} - ${response.species}`)
+    card.appendChild(pStatus)
+
+    let pLocation = document.createElement('p')
+
+    let spanLocation = document.createElement('span')
+    spanLocation.classList.add('location')
+    spanLocation.append('Last know location: ')
+    pLocation.appendChild(spanLocation)
+    pLocation.append(response.location.name)
+    card.appendChild(pLocation)
+
+    container.appendChild(card)
+}
+
+async function newFetch(url) {
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(response.status)
+
+    const json = await response.json()
+    return json
+}
+
+document.getElementById('characters').onclick = () => {
+    container.innerHTML = ''
+    for (let i = 1; i < 42; i++) {
+        newFetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+            .then(response => response.results.forEach(result => {
+                insertInPage(result)
+            }))
+            .catch(error => console.log(error))
     }
-   }
+}
 
-   async function locationFetch(url){
-       const response = await fetch(url)
 
-       if (!response.ok) throw new Error(response.status)
+document.getElementById('locations').onclick = () => {
+    container.innerHTML = ''
+    for (let i = 1; i <= 7; i++) {
+        newFetch(`https://rickandmortyapi.com/api/location?page=${i}`)
+            .then(response => response.results.forEach(result => {
+                setLocation(result)
+            }))
+            .catch(error => console.log(error))
+    }
+}
 
-       const location = await response.json()
-       return location
-   }
-
-   document.getElementById('locations').onclick = () => {
-       container.innerHTML = ''
-       for(let i = 1; i <= 7; i++){
-               locationFetch(`https://rickandmortyapi.com/api/location?page=${i}`)
-                .then(response => response.results.forEach(result => {
-                    setLocation(result)
-                }))
-                .catch(error => console.log(error))
-           }
-      }
-
-   function setLocation(location) {
+function setLocation(location) {
     let locationCard = document.createElement('div')
     locationCard.classList.add('location-card')
 
@@ -106,7 +97,7 @@ document.getElementById('characters').onclick = () => setAllCharacters()
     let pResidents = document.createElement('p')
     pResidents.classList.add('residents')
     pResidents.append(`Residents: ${location.residents.length} `)
-    
+
     let pResidentsButton = document.createElement('button')
     let iButton = document.createElement('i')
     iButton.classList.add('fas')
@@ -119,37 +110,41 @@ document.getElementById('characters').onclick = () => setAllCharacters()
     residentsContainer.classList.add('location-residents-container')
     location.residents.forEach(resident => {
         fetch(resident)
-        .then(res => res.json())
-        .then(json => {
-            let locationCharacterCard = document.createElement('div')
-            locationCharacterCard.classList.add('location-residents')
-            locationCharacterCard.classList.add('character-card')
+            .then(res => res.json())
+            .then(json => {
+                let locationCharacterCard = document.createElement('div')
+                locationCharacterCard.classList.add('location-residents')
+                locationCharacterCard.classList.add('character-card')
 
-            let img = document.createElement('img')
-            img.src = json.image
-            locationCharacterCard.appendChild(img)
+                let img = document.createElement('img')
+                img.src = json.image
+                locationCharacterCard.appendChild(img)
 
-            let h2 = document.createElement('h2')
-            h2.append(json.name)
-            locationCharacterCard.appendChild(h2)
+                let h2 = document.createElement('h2')
+                h2.append(json.name)
+                locationCharacterCard.appendChild(h2)
 
-            let p = document.createElement('p')
-            p.classList.add('status-wrapper')
-            let span = document.createElement('span')
-            span.classList.add('status')
-            span.classList.add(json.status)
-            p.appendChild(span)
-            p.append(`${json.status} - ${json.species}`)
-            locationCharacterCard.appendChild(p)
+                let p = document.createElement('p')
+                p.classList.add('status-wrapper')
+                let span = document.createElement('span')
+                span.classList.add('status')
+                span.classList.add(json.status)
+                p.appendChild(span)
+                p.append(`${json.status} - ${json.species}`)
+                locationCharacterCard.appendChild(p)
 
-            residentsContainer.appendChild(locationCharacterCard)
-        })
+                residentsContainer.appendChild(locationCharacterCard)
+            })
     })
     locationCard.appendChild(residentsContainer)
     container.appendChild(locationCard)
 
     pResidentsButton.onclick = () => {
-        residentsContainer.style.maxHeight == '0px' || residentsContainer.style.maxHeight == 0 ? residentsContainer.style.maxHeight = `${residentsContainer.scrollHeight}px` : residentsContainer.style.maxHeight = '0px'
+        if (residentsContainer.style.maxHeight == '0px' || residentsContainer.style.maxHeight == 0) {
+            residentsContainer.style.maxHeight = `${residentsContainer.scrollHeight}px`
+        } else {
+            residentsContainer.style.maxHeight = '0px'
+        }
         residentsContainer.classList.toggle('show')
 
         iButton.classList.toggle('fa-arrow-down')
