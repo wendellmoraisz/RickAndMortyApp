77b089fc9925f5213e63ace24a41c39313e4dc58
Wendell <wendellmorais3319@gmail.com>
2022-05-25 15:26:33 -0300

    refatoração do código: organização das funções e nomes mais claros para elas

--- scripts/script.js
+++ scripts/script.js
@@ -1,202 +1,229 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
-let charactersOfTheDay = []
-for (let i = 1; i < 42; i++) {
-    fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+(function getRandomCharacters() {
+    fetch(`https://rickandmortyapi.com/api/character/${Math.floor(Math.random() * 826)},${Math.floor(Math.random() * 826)},${Math.floor(Math.random() * 826)}`)
         .then(res => res.json())
-        .then(json => charactersOfTheDay.push(json))
-        .then(() => {
-            if (charactersOfTheDay.length == 41) {
-                for (let i = 0; i < 3; i++)
-                    insertInPage(charactersOfTheDay[Math.floor(Math.random() * 41)].results[Math.floor(Math.random() * 19)])
-            }
+        .then(json => {
+            for (let i in json) {
+                insertCharacterOnThePage(json[i]);
+            };
         })
-}
+        .catch(e => console.log(e));
+}());
 
-function insertInPage(response) {
-    let card = document.createElement('div')
-    card.classList.add('character-card')
+async function newRequest(url) {
+    const response = await fetch(url);
 
-    let img = document.createElement('img')
-    img.src = response.image
-    card.appendChild(img)
+    if (!response.ok) throw new Error(response.status);
 
-    let h2 = document.createElement('h2')
-    h2.textContent = response.name
-    card.appendChild(h2)
+    const json = await response.json();
+    return json;
+};
 
-    let pStatus = document.createElement('p')
-    pStatus.classList.add('status-wrapper')
-    let spanByStatus = document.createElement('span')
-    spanByStatus.classList.add('status')
-    spanByStatus.classList.add(response.status)
-    pStatus.appendChild(spanByStatus)
-    pStatus.append(`${response.status} - ${response.species}`)
-    card.appendChild(pStatus)
+async function getAllCharacters() {
+    container.innerHTML = '';
+    const data = await newRequest('https://rickandmortyapi.com/api/character');
+    let { pages } = data.info;
+    let endpoints = [];
 
-    let pLocation = document.createElement('p')
+    for (let i = 1; i < pages + 1; i++) {
+        endpoints.push(`https://rickandmortyapi.com/api/character?page=${i}`);
+    };
 
-    let spanLocation = document.createElement('span')
-    spanLocation.classList.add('location')
-    spanLocation.append('Last know location: ')
-    pLocation.appendChild(spanLocation)
-    pLocation.append(response.location.name)
-    card.appendChild(pLocation)
+    async function setCharacters() {
+        const requests = endpoints.map(url => fetch(url).then(res => res.json()));
 
-    container.appendChild(card)
-}
+        Promise.all(requests)
+            .then(response => {
+                response.forEach(e => {
+                    e.results.forEach(character => insertCharacterOnThePage(character));
+                });
+            })
+            .catch(e => console.log(e.message));
+    };
 
-async function newFetch(url) {
-    const response = await fetch(url)
+    await setCharacters();
+};
 
-    if (!response.ok) throw new Error(response.status)
+async function getLocations() {
+    container.innerHTML = '';
+    const data = await newRequest('https://rickandmortyapi.com/api/location');
+    let { count } = data.info;
+    let endpoints = [];
 
-    const json = await response.json()
-    return json
-}
+    for (let i = 1; i < count + 1; i++) {
+        endpoints.push(`https://rickandmortyapi.com/api/location/${i}`);
+    };
+    const requests = endpoints.map(url => fetch(url).then(res => res.json()));
 
-document.getElementById('characters').onclick = () => {
-    container.innerHTML = ''
-    for (let i = 1; i < 42; i++) {
-        newFetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-            .then(response => response.results.forEach(result => {
-                insertInPage(result)
-            }))
-            .catch(error => console.log(error))
-    }
-}
+    Promise.all(requests)
+        .then(response => {
+            response.map(location => insertLocationOnThePage(location));
+        })
+        .catch(e => console.log(e.message));
+};
 
-document.getElementById('locations').onclick = () => {
-    container.innerHTML = ''
-    for (let i = 1; i <= 7; i++) {
-        newFetch(`https://rickandmortyapi.com/api/location?page=${i}`)
-            .then(response => response.results.forEach(result => {
-                setLocation(result)
-            }))
-            .catch(error => console.log(error))
-    }
-}
+async function getEpisodes() {
+    container.innerHTML = '';
 
-function setLocation(location) {
-    let locationCard = document.createElement('div')
-    locationCard.classList.add('main-card')
+    for (let i = 1; i <= 3; i++) {
+        newRequest(`https://rickandmortyapi.com/api/episode?page=${i}`)
+            .then(res => res.results.forEach(episode => insertEpisodesOnThePage(episode)))
+            .catch(e => console.log(e.message));
+    };
+};
 
-    let h1 = document.createElement('h1')
-    h1.append(location.name)
-    locationCard.appendChild(h1)
+function insertCharacterOnThePage(response) {
+    let card = document.createElement('div');
+    card.classList.add('character-card');
 
-    let pType = document.createElement('p')
-    pType.append(`Type: ${location.type}`)
-    locationCard.appendChild(pType)
+    let img = document.createElement('img');
+    img.alt = `${response.name} character photo`;
+    img.src = response.image;
+    card.appendChild(img);
 
-    let pDimension = document.createElement('p')
-    pDimension.append(`Dimension: ${location.dimension}`)
-    locationCard.appendChild(pDimension)
+    let h2 = document.createElement('h2');
+    h2.textContent = response.name;
+    card.appendChild(h2);
 
-    let pResidents = document.createElement('p')
-    pResidents.classList.add('residents')
-    pResidents.append(`Residents: ${location.residents.length} `)
+    let pStatus = document.createElement('p');
+    pStatus.classList.add('status-wrapper');
+    let spanByStatus = document.createElement('span');
+    spanByStatus.classList.add('status');
+    spanByStatus.classList.add(response.status);
+    pStatus.appendChild(spanByStatus);
+    pStatus.append(`${response.status} - ${response.species}`);
+    card.appendChild(pStatus);
 
-    let pResidentsButton = document.createElement('button')
-    let iButton = document.createElement('i')
-    iButton.classList.add('fas')
-    iButton.classList.add('fa-arrow-down')
-    pResidentsButton.appendChild(iButton)
-    pResidents.appendChild(pResidentsButton)
-    locationCard.appendChild(pResidents)
+    let pLocation = document.createElement('p');
 
-    let residentsContainer = document.createElement('div')
-    residentsContainer.classList.add('main-characters-container')
-    location.residents.forEach(resident => fetchCharacter(resident, residentsContainer))
+    let spanLocation = document.createElement('span');
+    spanLocation.classList.add('location');
+    spanLocation.append('Last know location: ');
+    pLocation.appendChild(spanLocation);
+    pLocation.append(response.location.name);
+    card.appendChild(pLocation);
 
+    container.appendChild(card);
+};
 
-    locationCard.appendChild(residentsContainer)
-    container.appendChild(locationCard)
-    buttonInteration(pResidentsButton, iButton, residentsContainer)
-}
-async function fetchCharacter(characterurl, container){
+function insertLocationOnThePage(location) {
+    let locationCard = document.createElement('div');
+    locationCard.classList.add('main-card');
+
+    let h1 = document.createElement('h1');
+    h1.append(location.name);
+    locationCard.appendChild(h1);
+
+    let pType = document.createElement('p');
+    pType.append(`Type: ${location.type}`);
+    locationCard.appendChild(pType);
+
+    let pDimension = document.createElement('p');
+    pDimension.append(`Dimension: ${location.dimension}`);
+    locationCard.appendChild(pDimension);
+
+    let pResidents = document.createElement('p');
+    pResidents.classList.add('residents');
+    pResidents.append(`Residents: ${location.residents.length} `);
+
+    let pResidentsButton = document.createElement('button');
+    let iButton = document.createElement('i');
+    iButton.classList.add('fas');
+    iButton.classList.add('fa-arrow-down');
+    pResidentsButton.appendChild(iButton);
+    pResidents.appendChild(pResidentsButton);
+    locationCard.appendChild(pResidents);
+
+    let residentsContainer = document.createElement('div');
+    residentsContainer.classList.add('main-characters-container');
+    location.residents.forEach(resident => fetchCharacter(resident, residentsContainer));
+
+    locationCard.appendChild(residentsContainer);
+    container.appendChild(locationCard);
+    buttonInteration(pResidentsButton, iButton, residentsContainer);
+};
+
+async function fetchCharacter(characterurl, container) {
     await fetch(characterurl)
         .then(res => res.json())
         .then(json => {
-            let locationCharacterCard = document.createElement('div')
-            locationCharacterCard.classList.add('main-characters')
-            locationCharacterCard.classList.add('character-card')
+            let locationCharacterCard = document.createElement('div');
+            locationCharacterCard.classList.add('main-characters');
+            locationCharacterCard.classList.add('character-card');
 
-            let img = document.createElement('img')
-            img.src = json.image
-            locationCharacterCard.appendChild(img)
+            let img = document.createElement('img');
+            img.src = json.image;
+            img.alt = `${json.name} character photo`;
+            locationCharacterCard.appendChild(img);
 
-            let h2 = document.createElement('h2')
-            h2.append(json.name)
-            locationCharacterCard.appendChild(h2)
+            let h2 = document.createElement('h2');
+            h2.append(json.name);
+            locationCharacterCard.appendChild(h2);
 
-            let p = document.createElement('p')
-            p.classList.add('status-wrapper')
-            let span = document.createElement('span')
-            span.classList.add('status')
-            span.classList.add(json.status)
-            p.appendChild(span)
-            p.append(`${json.status} - ${json.species}`)
-            locationCharacterCard.appendChild(p)
+            let p = document.createElement('p');
+            p.classList.add('status-wrapper');
+            let span = document.createElement('span');
+            span.classList.add('status');
+            span.classList.add(json.status);
+            p.appendChild(span);
+            p.append(`${json.status} - ${json.species}`);
+            locationCharacterCard.appendChild(p);
 
-            container.appendChild(locationCharacterCard)
-        })
-    }
+            container.appendChild(locationCharacterCard);
+        });
+};
+
+function insertEpisodesOnThePage(episode) {
+    let episodeContainer = document.createElement('div');
+    episodeContainer.classList.add('main-card');
+
+    let h1 = document.createElement('h3');
+    h1.innerHTML = `${episode.episode} - ${episode.name}`;
+    episodeContainer.appendChild(h1);
+
+    let releaseDate = document.createElement('p');
+    releaseDate.innerHTML = `Release date of: ${episode.air_date}`;
+    episodeContainer.appendChild(releaseDate);
+
+    let p = document.createElement('p');
+    p.classList.add('residents');
+    p.innerHTML = `Participants: ${episode.characters.length}`;
+    episodeContainer.appendChild(p);
+    
+    let button = document.createElement('button');
+    let iButton = document.createElement('i');
+    iButton.classList.add('fas');
+    iButton.classList.add('fa-arrow-down');
+    button.appendChild(iButton);
+    p.appendChild(button);
+    
+    let participantsContainer = document.createElement('div');
+    participantsContainer.classList.add('main-characters-container');
+    episodeContainer.appendChild(participantsContainer);
+    
+    episode.characters.forEach(character => fetchCharacter(character, participantsContainer));
+
+    container.appendChild(episodeContainer);
+    buttonInteration(button, iButton, participantsContainer);
+};
 
 function buttonInteration(button, iButton, elementContainer) {
     button.onclick = () => {
         if (elementContainer.style.maxHeight == '0px' || elementContainer.style.maxHeight == 0) {
-            elementContainer.style.maxHeight = `${elementContainer.scrollHeight}px`
+            elementContainer.style.maxHeight = `${elementContainer.scrollHeight}px`;
 
         } else {
-            elementContainer.style.maxHeight = '0px'
-        }
+            elementContainer.style.maxHeight = '0px';
+        };
 
-        elementContainer.classList.toggle('show')
+        elementContainer.classList.toggle('show');
 
-        iButton.classList.toggle('fa-arrow-down')
-        iButton.classList.toggle('fa-arrow-up')
-    }
-}
+        iButton.classList.toggle('fa-arrow-down');
+        iButton.classList.toggle('fa-arrow-up');
+    };
+};
 
-document.getElementById('episodes').onclick = () => {
-    container.innerHTML = ''
-
-    for (let i = 1; i <= 3; i++) {
-        newFetch(`https://rickandmortyapi.com/api/episode?page=${i}`)
-            .then(res => res.results.forEach(episode => {
-                let episodeContainer = document.createElement('div')
-                episodeContainer.classList.add('main-card')
-
-                let h1 = document.createElement('h3')
-                h1.innerHTML = `${episode.episode} - ${episode.name}`
-                episodeContainer.appendChild(h1)
-
-                
-                let releaseDate = document.createElement('p')
-                releaseDate.innerHTML = `Release date of: ${episode.air_date}`
-                episodeContainer.appendChild(releaseDate)
-
-                let p = document.createElement('p')
-                p.classList.add('residents')
-                p.innerHTML = `Participants: ${episode.characters.length}`
-                episodeContainer.appendChild(p)
-                
-                let button = document.createElement('button')
-                let iButton = document.createElement('i')
-                iButton.classList.add('fas')
-                iButton.classList.add('fa-arrow-down')
-                button.appendChild(iButton)
-                p.appendChild(button)
-                
-                let participantsContainer = document.createElement('div')
-                participantsContainer.classList.add('main-characters-container') //
-                episodeContainer.appendChild(participantsContainer)
-                
-                episode.characters.forEach(character => fetchCharacter(character, participantsContainer))
-                
-                container.appendChild(episodeContainer)
-                buttonInteration(button, iButton, participantsContainer)
-            }))
-    }
-}
+document.getElementById('characters').addEventListener('click', getAllCharacters);
+document.getElementById('locations').addEventListener('click', getLocations);
+document.getElementById('episodes').addEventListener('click', getEpisodes);
